547cf928700e554ea48238b92e5ef70f71c6883d
karancsi <36573204+karancsi@users.noreply.github.com>
2018-05-17 21:00:29 +0200

    WPF_02
    elvileg ez a jó

--- MainChoose.xaml.cs
+++ MainChoose.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp3
+namespace WpfApp2
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,11 +24,33 @@ namespace WpfApp3
         public MainWindow()
         {
             InitializeComponent();
+            Lion l1 = new Lion();
+            Lion l2 = new Lion();
+
+
+
+
+            Console.WriteLine(l1.Hp);
+            Console.WriteLine(l2.Hp);
+            l1.Attack(l2);
+            Console.WriteLine(l1.Hp);
+            Console.WriteLine(l2.Hp);
+            Console.WriteLine("Luv ya");
+//Szirszar.Content = l1.Hp;
+
+
+        }
+
+      
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You choose: Lion", "Choose");
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Do you want to quit? Really?","Quit", MessageBoxButton.YesNo);
+            MessageBox.Show("You choose: Gazelle", "Choose");
         }
     }
 }
