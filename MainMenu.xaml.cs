be52c0d078871b1157575dc2832f31acbb3d0862
karancsi <36573204+karancsi@users.noreply.github.com>
2018-05-17 20:52:14 +0200

    WPF
    Try

---
+++ MainMenu.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const byte MENU = 0, NEWGAME = 1, MUSIC = 2, OPTIONS = 3, QUIT = 4;
+        int CurrentScreen = MENU;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Do you want to quit? Really?","Quit", MessageBoxButton.YesNo);
+        }
+
+
+    }
+}
